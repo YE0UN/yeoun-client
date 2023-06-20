f45a36401cd761bf1b73700ec4628293a51b860d
bizbaeja <bjgbjg112@gmail.com>
2023-06-20 13:49:19 +0900

    design: global.css ,Button.jsx 변수 수정 (#1)

--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -3,53 +3,51 @@ const SIZES = {
   sm: css`
     --button-width: 6.5rem;
     --button-height: 3.5rem;
-    --button-font-size: 16px;
+    --button-font-size: var(--fs-sm);
     --button-radius: 8px;
   `,
   md: css`
     --button-width: 10rem;
-    --button-font-size: 16px;
+    --button-font-size: var(--fs-sm);
     --button-height: 4.5rem;
     --button-radius: 8px;
   `,
   lg: css`
     --button-width: 10rem;
     --button-height: 5.4rem;
-    --button-font-size: 20px;
+    --button-font-size: var(--fs-lg);
     --button-radius: 8px;
   `,
   xl: css`
     --button-width: 45rem;
     --button-height: 6rem;
-    --button-font-size: 20px;
+    --button-font-size: var(--fs-lg);
     --button-radius: 8px;
   `,
 };
 
 const VARIANTS = {
-  tab: css`
+  main: css`
+    --button-color: #ffffff;
+    --button-bg-color: #577fa0;
+    --button-hover-bg-color: #94afc6;
+  `,
+  region: css`
     --button-color: #ffffff7;
     --button-bg-color: #767676;
     --button-hover-bg-color: #c4c4c4;
   `,
-  error: css`
-    --button-color: #ffffff;
-    --button-bg-color: #e23737;
-    --button-hover-bg-color: #c82333;
-  `,
 };
-
-function Button({ disabled, size, variants, children }) {
+const Button = ({ disabled, size, variants, children }) => {
   const styleSize = SIZES[size];
   const variantStyle = VARIANTS[variants];
 
-  console.log(variantStyle);
   return (
     <MainBtn styleSize={styleSize} variantStyle={variantStyle} disabled={disabled}>
       {children}
     </MainBtn>
   );
-}
+};
 
 export default Button;
 
