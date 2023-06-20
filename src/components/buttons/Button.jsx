56705b581eb83cfd3eb67a02f1a5fcd58c4cd315
bizbaeja <bjgbjg112@gmail.com>
2023-06-20 09:45:13 +0900

    style: VARIANTS 변수  추가 (#1)

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,40 +1,4 @@
 import styled, { css } from 'styled-components';
-
-export const Button = ({ disabled, size, variants, children }) => {
-  const styleSize = SIZES[size];
-  const variantStyle = VARIANTS[variants];
-  return (
-    <MainBtn styleSize={styleSize} variantStyle={variantStyle} disabled={disabled}>
-      {children}
-    </MainBtn>
-  );
-};
-
-const MainBtn = styled.button`
-  ${(p) => p.styleSize}
-  ${(p) => p.variantStyle}
-
-  background-color: var(--main-btn-color);
-  width: var(--button-width);
-  height: 60px;
-  color: var(--main-btn-text-color);
-  border-radius: 8px;
-  font-size: var(--button-font-size);
-  padding: var(--button-padding);
-  margin: var(--button-margin);
-
-  &:active,
-  &:focus,
-  &:hover {
-    background-color: var(--main-btn-hover-color);
-  }
-
-  &:disabled {
-    cursor: default;
-    background-color: var(--main-btn-disabled-color);
-  }
-`;
-
 const SIZES = {
   sm: css`
     --button-width: 6.5rem;
@@ -64,7 +28,9 @@ const SIZES = {
 
 const VARIANTS = {
   tab: css`
-    --button-color: #767676;
+    --button-color: #ffffff7;
+    --button-bg-color: #767676;
+    --button-hover-bg-color: #c4c4c4;
   `,
   error: css`
     --button-color: #ffffff;
@@ -72,3 +38,39 @@ const VARIANTS = {
     --button-hover-bg-color: #c82333;
   `,
 };
+
+export const Button = ({ disabled, size, variants, children }) => {
+  const styleSize = SIZES[size];
+  const variantStyle = VARIANTS[variants];
+
+  console.log(variantStyle);
+  return (
+    <MainBtn styleSize={styleSize} variantStyle={variantStyle} disabled={disabled}>
+      {children}
+    </MainBtn>
+  );
+};
+
+const MainBtn = styled.button`
+  ${(p) => p.styleSize}
+  ${(p) => p.variantStyle}
+  background-color: var(--button-bg-color,--main-btn-color);
+  width: var(--button-width);
+  height: 60px;
+  color: var(--main-btn-text-color, #ffffff);
+  border-radius: 8px;
+  font-size: var(--button-font-size);
+  padding: var(--button-padding);
+  margin: var(--button-margin);
+
+  &:active,
+  &:focus,
+  &:hover {
+    background-color: var(--button-hover-bg-color, --main-btn-hover-color);
+  }
+
+  &:disabled {
+    cursor: default;
+    background-color: var(--main-btn-disabled-color);
+  }
+`;
