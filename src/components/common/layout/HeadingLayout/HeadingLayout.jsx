c2f174578d9db0aaccbae87c632f3a478c02778f
euiho.kim <kim-eh@naver.com>
2023-06-28 21:18:15 +0900

    style: 마이 페이지 및 프로필 설정 페이지를 위한 코드 추가 (#49)

--- src/components/common/layout/HeadingLayout/HeadingLayout.jsx
+++ src/components/common/layout/HeadingLayout/HeadingLayout.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const HeadingLayout = ({ heading }) => {
+const HeadingLayout = ({ heading, subHeading }) => {
   return (
     <>
       <section>
-        <H2>{heading}</H2>
+        <Wrapper>
+          <H2>{heading}</H2>
+          {subHeading ? <Anchor href='/profile/edit'>{subHeading}</Anchor> : <></>}
+        </Wrapper>
       </section>
     </>
   );
@@ -13,10 +16,22 @@ const HeadingLayout = ({ heading }) => {
 
 export default HeadingLayout;
 
+const Wrapper = styled.div`
+  display: flex;
+  gap: 5rem;
+  border-bottom: 1px solid var(--border-color);
+`;
+
 const H2 = styled.h2`
   font-size: var(--fs-4xl);
   font-weight: 700;
   margin-top: 9rem;
   padding-bottom: 1.6rem;
-  border-bottom: 1px solid var(--border-color);
+`;
+
+const Anchor = styled.a`
+  font-size: var(--fs-md);
+  font-weight: 500;
+  margin-top: 10rem;
+  color: var(--sub-text-color);
 `;
