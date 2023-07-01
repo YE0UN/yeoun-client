9cf75c1e4c2f1a38e089fdc622f3354bbce9469c
euiho.kim <kim-eh@naver.com>
2023-07-01 22:29:29 +0900

    style: 커스텀 훅 주석 추가 (#59)

--- src/hooks/useModal.jsx
+++ src/hooks/useModal.jsx
@@ -2,8 +2,8 @@ import { useState, useEffect, useRef } from 'react';
 
 const useModal = () => {
   const [modalOpen, setModalOpen] = useState(false);
-  const firstRef = useRef(null);
-  const secondRef = useRef(null);
+  const firstRef = useRef(null); // 모달 open & close 버튼
+  const secondRef = useRef(null); // 모달 내용
 
   const toggle = () => {
     setModalOpen((cur) => !cur);
@@ -11,6 +11,7 @@ const useModal = () => {
 
   useEffect(() => {
     const handleOutsideClick = (event) => {
+      // 모달이 열려 있을 때 && 모달의 open & close 버튼을 클릭하지 않았을 때 && 모달 내용 영역을 클릭하지 않았을 때
       if (modalOpen && !firstRef.current.contains(event.target) && !secondRef.current.contains(event.target)) {
         toggle();
       }
