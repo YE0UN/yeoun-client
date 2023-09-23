baf302531526fe3bc87bf9c5d5342cae8271272f
euiho.kim <kim-eh@naver.com>
2023-09-23 21:43:53 +0900

    style: Global및 Local SVG Sprite 추가에 따른 수정 (#119)

---
+++ src/components/SVGSprite/LocalSVGSprite.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const SVGSprite = ({
+  id,
+  color,
+  width = '100%',
+  height = '100%',
+  ariaLabel,
+  onClickHandler,
+  $ref,
+  cursor = 'pointer',
+}) => {
+  return (
+    <svg
+      fill={color}
+      width={width}
+      height={height}
+      aria-label={ariaLabel}
+      onClick={onClickHandler}
+      ref={$ref}
+      style={{ cursor: `${cursor}` }}
+    >
+      <use href={`#${id}`} />
+    </svg>
+  );
+};
+
+export default SVGSprite;
